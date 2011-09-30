6507535b11dec59a2d36f8be556c8ecc85c52698
Matthew Richmond <matthew@choppingblock.com>
2011-09-29 21:50:20 -0400

    Created loadJSONGalleryProject

---
+++ loadJSONGalleryProject/gallery.js
@@ -0,0 +1,22 @@
+galleryLoadHandler(
+	{
+	title:"Sample Images",
+	photos:[
+			{
+				title:"A Fish",
+				description:"Oh, that is cool.",
+				url:"images/mbr_090409_8318.jpg"
+			},
+			{
+				title:"Grass",
+				description:"Mmm, grass...",
+				url:"images/mbr_090412_8583.jpg"
+			},
+			{
+				title:"Sad Robot",
+				description:"So cute, so sad.",
+				url:"images/mbr_090820_2220.jpg"
+			}
+		]
+	}
+)
