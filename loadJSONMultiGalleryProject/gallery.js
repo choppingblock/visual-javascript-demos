969e2c4fb7b6919caf59873bb1e68fb4f2a08c0b
Matthew Richmond <matthew@choppingblock.com>
2011-10-07 10:06:31 -0400

    Created loadJSONMultiGalleryProject

---
+++ loadJSONMultiGalleryProject/gallery.js
@@ -0,0 +1,75 @@
+galleryLoadHandler(
+	{
+	title:"Sample Images",
+	sets:[
+		{
+			title:"Set 1: A really cool thing",
+			display_name:"Set 1",
+			photos:[
+				{
+					title:"A Fish",
+					description:"Oh, that is cool.",
+					url:"images/mbr_090409_8318.jpg"
+				},
+				{
+					title:"More Fish",
+					description:"Mmm, grass...",
+					url:"images/mbr_090409_8326.jpg"
+				},
+				{
+					title:"Grass",
+					description:"So cute, so sad.",
+					url:"images/mbr_090412_8583.jpg"
+				}
+			]
+		},
+		{
+			title:"Set 2: More Super Awesome",
+			display_name:"Set 2",
+			photos:[
+				{
+					title:"Forest",
+					description:"Oh, that is cool.",
+					url:"images/mbr_090419_8616.jpg"
+				},
+				{
+					title:"A",
+					description:"Mmm, grass...",
+					url:"images/mbr_090510_9067.jpg"
+				},
+				{
+					title:"M",
+					description:"So cute, so sad.",
+					url:"images/mbr_090510_9071.jpg"
+				}
+			]
+		},
+		{
+			title:"Set 3: Even More Super Awesome",
+			display_name:"Set 3",
+			photos:[
+				{
+					title:"Forest",
+					description:"Oh, that is cool.",
+					url:"images/mbr_090510_9073.jpg"
+				},
+				{
+					title:"Wall",
+					description:"Mmm, grass...",
+					url:"images/mbr_090523_9115.jpg"
+				},
+				{
+					title:"Wall 2",
+					description:"So cute, so sad.",
+					url:"images/mbr_090523_9116.jpg"
+				},
+				{
+					title:"2",
+					description:"So cute, so sad.",
+					url:"images/mbr_090523_9123.jpg"
+				}
+			]
+		}
+	]
+	}
+)	
