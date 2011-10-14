edaa6748f578521d9c74802c333a4b095a71d6cd
Matthew Richmond <matthew@choppingblock.com>
2011-10-13 22:07:00 -0400

    Files From Class October 13

---
+++ loadJSONHighlightThumbnailGalleryProject/gallery.js
@@ -0,0 +1,27 @@
+galleryLoadHandler(
+	{
+	title:"Sample Images",
+	photos:[
+			{
+				title:"A Fish",
+				description:"Oh, that is cool.",
+				url:"mbr_090409_8318.jpg"
+			},
+			{
+				title:"Grass",
+				description:"Mmm, grass...",
+				url:"mbr_090412_8583.jpg"
+			},
+			{
+				title:"Elephant",
+				description:"So cute, so sad.",
+				url:"mbr_090523_9115.jpg"
+			},
+			{
+				title:"Two",
+				description:"So cute, so sad.",
+				url:"mbr_090523_9123.jpg"
+			}
+		]
+	}
+)
